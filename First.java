f47372ad2ea741d6242dd953b5199460526af41c
devesh01007 <devesh500069565@stu.upes.ac.in>
2020-12-31 19:36:26 +0530

    first commit

---
+++ First.java
@@ -0,0 +1,23 @@
+import java.lang.*;
+ import java. until.*;
+
+           class Keyboard
+              {  
+                              public static void main(String args[ ])
+                          
+                              {
+                                 
+                       
+                              scanner s =new scanner (System.in);
+
+                                String name;
+
+                             System.out.println("May I Know your name");
+         
+                          name= s .nextLine( );
+ 
+                             System.out.println("Welcome" +name);
+    
+              }
+  
+                            }
---
+++ MyFirst.java
@@ -0,0 +1,16 @@
+import java. lang.*;
+
+ 
+     class MyFirst
+
+     
+      {
+
+               public static void main(String args[])
+               {
+            
+                                
+                        System.out.println("Hello World");
+               }    
+                 
+      }    
